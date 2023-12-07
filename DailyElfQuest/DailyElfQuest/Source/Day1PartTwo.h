d263b2677964c31198d4735a9805a4e6e73e9183
PhilippeHo27 <0446386@bdeb.qc.ca>
2023-12-07 13:02:56 -0500

    Utilities
    From NuMakesGames / Advent-of-code-2023. Updated to C++ 2023, added correct paths, will fix root in next push

--- DailyElfQuest/DailyElfQuest/Source/Day1PartTwo.h
+++ DailyElfQuest/DailyElfQuest/Source/Day1PartTwo.h
@@ -7,7 +7,5 @@ public:
 	Day1 Day1;
 	void SolutionDayTwo();
 	int ConcatenateTwoDigits(int digit1, int digit2);
-	//map<string, int> stringToNumber = { {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9} };
-
 };
 
